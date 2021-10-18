115efab0dc6a32a097329d9a42941a62665cb847
Carlos Giner <carlosginer90@gmail.com>
2021-10-18 12:02:44 -0400

    finished layout responsive

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const logo = document.querySelector('.header__logo--img');
+const imgLogo = document.querySelector('.header__logo img');
+const desktopNavContainer = document.querySelector('.desk__container--nav');
+const navUl = document.querySelector('.nav ul');
 const header = document.querySelector('header');
 
 const headerOptions = {};
@@ -8,7 +10,15 @@ const headerObserver = new IntersectionObserver(
 function(entries,headerObserver){
 
     entries.forEach(entry=>{
-        console.log(entry.target)
+        if (!entry.isIntersecting) {
+            imgLogo.classList.add('shrink');
+            desktopNavContainer.classList.add('changeBackgroundColor');
+            navUl.classList.add('makeTransparent');
+        }else {
+            imgLogo.classList.remove('shrink');
+            desktopNavContainer.classList.remove('changeBackgroundColor')
+            navUl.classList.remove('makeTransparent');
+        }
     })
 },headerOptions);
 
